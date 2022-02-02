93c64804941c2c660c9ebffb79aa13580a8b6eda
Johannes Fröhlich <jfroehlich@users.noreply.github.com>
2022-02-02 10:13:02 +0100

    Added the code to find the components.

--- index.js
+++ index.js
@@ -4,6 +4,80 @@ const glob = require("glob");
 const fs = require("fs");
 const path = require("path");
 
+async function findFiles(pattern, options) {
+	return new Promise((resolve, reject) => {
+		glob(pattern, options, (error, files) => {
+			if (error) {
+				return reject(error);
+			}
+			resolve(files);
+		});
+	});
+}
+
+async function findComponents(options) {
+	const config = Object.assign({
+		componentDir: "./assets",
+		templateExtensions: "njk,hbs,html",
+		ignore: []
+	}, options);
+
+	const components = {};
+	const pattern = `**/*.{${config.templateExtensions}}`;
+
+	// Finds the files provided by the pattern and ignores variants -- they are handled below.
+	// Variant templates should have the parents context, but they won't have it if added here directly.
+	const templates = await findFiles(pattern, {cwd: config.componentDir, ignore: config.ignore.concat(["**/*--*.*"])});
+	for (const file of templates) {
+		let handle = path.basename(file, path.extname(file));
+		const comp = {
+			path: `${file}`,
+			ctx: {}
+		};
+
+		// TODO Move this part to it's own function
+		const configFiles = await findFiles(`${path.dirname(file)}/${handle}.config.{yml,yaml}`, {cwd: config.componentDir});
+		if (configFiles.length > 0) {
+			const cf = fs.readFileSync(path.posix.join(config.componentDir, configFiles[0]), "utf-8");
+			const compConfig = YAML.parse(cf);
+
+			if (!!compConfig === true) {
+				// Fetch the context from the configuration. That's we why do all this.
+				comp.ctx = !!compConfig.context === true ? Object.assign(comp.ctx, compConfig.context) : comp.ctx;
+
+				// The handle can be overriden in the config.
+				handle = !!compConfig.handle === true ? compConfig.handle : handle;
+
+				// Finding the variants
+				if (compConfig.variants) {
+					for (const variant of compConfig.variants) {
+						if (!!variant.name === false) {
+							continue;
+						}
+						
+						// Clone the parents context and merge the variants context if it exists.
+						const vcomp = structuredClone(comp); // requires min nodejs 17.0, but does an actual deep clone
+						vcomp.ctx = !!variant.context === true ? Object.assign(vcomp.ctx, variant.context) : vcomp.ctx;
+						
+						// Check if there is a template file for this variant
+						const vtempls = await findFiles(`${path.dirname(file)}/${handle}--${variant.name}.{${config.templateExtensions}}`, {cwd: config.componentDir}); 
+						if (vtempls.length > 0) {
+							vcomp.path = vtempls[0];
+						}
+
+						components[`@${handle}--${variant.name}`] = vcomp;
+					}
+				}
+			}
+			
+		}
+
+		// Add the file to the components list
+		components[`@${handle}`] = comp;
+	}
+	return components;
+}
+
 const TAG_NAME = "component";
 
 module.exports = class ComponentExtension {
